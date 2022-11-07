eb78cdaec57b643953ffbd1f4551ae9ff8310127
yetemegn-telaye <lemma19yet@gmail.com>
2022-11-07 22:36:43 +0300

    Update checked status of todo items

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -22,11 +22,26 @@ class TodoContainer extends React.Component {
           }
         ]
        };
+       handleChange = (id)=> {
+        this.setState(prevState=>{
+            return {
+                todos: prevState.todos.map(todo=>{
+                    if(todo.id === id){
+                        return{
+                            ...todo,
+                            completed: !todo.completed
+                        }
+                    }
+                    return todo;
+                }),
+            }
+        })
+       }
     render(){
         return(
             <div>
                 <Header/>
-                <TodoList todos={this.state.todos}/>
+                <TodoList todos={this.state.todos} handleChangeProps={this.handleChange}/>
             </div>
         )
     }
